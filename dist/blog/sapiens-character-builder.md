2b6535200ab110387821618837fbcf96429c6826
AmitDeka <amitdek49@gmail.com>
2024-08-03 16:59:05 +0530

    Feature: Added Netlify CMS

---
+++ dist/blog/sapiens-character-builder.md
@@ -0,0 +1,9 @@
+---
+name: Sapiens Character Builder
+tags: "post"
+backgroundColor: 1f2f3f
+image: /assets/images/sapiens.ui8.net.svg
+imageAlt: Sapiens Character Builder Icon
+description: Sapiens is a beautiful library of modular, component-based character illustrations for Sketch and Figma.
+link: https://sapiens.ui8.net
+---
--- eleventy.config.js
+++ eleventy.config.js
@@ -1,8 +1,9 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./dist/assets/");
+  eleventyConfig.addPassthroughCopy("./dist/admin/");
   return {
-    templateFormats: ["md", "njk"],
-    htmlTemplateEngine: "njk",
+    // templateFormats: ["md", "njk"],
+    // htmlTemplateEngine: ["md", "njk"],
     dir: {
       input: "dist/",
       output: "./public",
