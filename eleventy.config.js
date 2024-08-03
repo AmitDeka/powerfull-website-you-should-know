5843717a4637324be4b8ff201246b6789366a566
AmitDeka <amitdek49@gmail.com>
2024-08-03 19:15:19 +0530

    Final update

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,8 +2,8 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./dist/assets/");
   eleventyConfig.addPassthroughCopy("./dist/admin/");
   return {
-    // templateFormats: ["md", "njk"],
-    // htmlTemplateEngine: ["md", "njk"],
+    templateFormats: ["md", "njk"],
+    htmlTemplateEngine: ["md", "njk"],
     dir: {
       input: "dist/",
       output: "./public",
