5b719915700e6453470fc47931d4d2aab1831bfc
AmitDeka <amitdek49@gmail.com>
2024-08-04 10:53:47 +0530

    Google site verification

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,8 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./dist/assets/");
   eleventyConfig.addPassthroughCopy("./dist/admin/");
+  eleventyConfig.addPassthroughCopy("./dist/google82a88c0b668c979c.html");
+
   return {
     templateFormats: ["md", "njk"],
     htmlTemplateEngine: ["md", "njk"],
