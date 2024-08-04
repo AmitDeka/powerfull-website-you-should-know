437047c7d18bba55acad388ee58c5d3c65f63ca9
AmitDeka <amitdek49@gmail.com>
2024-08-04 17:39:22 +0530

    Feature: Add more specific curation

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,6 +3,10 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./dist/admin/");
   eleventyConfig.addPassthroughCopy("./dist/google82a88c0b668c979c.html");
 
+  eleventyConfig.addFilter("limit", function (arr, limit) {
+    return arr.slice(0, limit);
+  });
+
   return {
     templateFormats: ["md", "njk"],
     htmlTemplateEngine: ["md", "njk"],
