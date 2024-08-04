6e3a8a795588dab60d9ce4281513f1b54d727488
AmitDeka <amitdek49@gmail.com>
2024-08-04 23:20:55 +0530

    Bug Fix #6

--- dist/assets/js/catTag.js
+++ dist/assets/js/catTag.js
@@ -1,21 +1,14 @@
-console.log("Custom script loaded");
-
 CMS.registerEventListener({
   name: "preSave",
   handler: ({ entry }) => {
     const data = entry.get("data").toJS();
     const category = data.category;
-    let tags = data.tags || [];
-
-    console.log("Category:", category);
-    console.log("Tags before:", tags);
+    const tags = data.tags || [];
 
     if (category && !tags.includes(category)) {
       tags.push(category);
       entry = entry.setIn(["data", "tags"], tags);
     }
-
-    console.log("Tags after:", tags);
 
     return entry;
   },
